1426a83a0217bf341edc1affb4130b3254c8c84d
Pavi Singh <pavisingh236@gmail.com>
2022-01-03 11:42:33 -0800

    added robot name

---
+++ assets/js/game.js
@@ -0,0 +1,12 @@
+var playerName = window.prompt("What is your robot's name?");
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is tjis?
+console.log("Our robot's name is " + playerName);
+
+// this creates a funtion named "fight"
+function fight () {
+    window.alert("The fight has begun!");
+}
+// fight();
