d643f02013842a462462f95d6fe3e7e85c6c305f
Pavi Singh <pavisingh236@gmail.com>
2022-01-03 17:27:56 -0800

    add enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -44,7 +44,7 @@ function fight() {
         window.alert(playerName + " still has " + playerHealth + " health left.");
     }
     // if player choses to skip
-    } else-if (promptFight === "skip" || promptFight === "SKIP") {
+    } else if (promptFight === "skip" || promptFight === "SKIP") {
     // confirm player wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
   
